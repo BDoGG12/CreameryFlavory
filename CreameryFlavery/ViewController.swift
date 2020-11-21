a92a6c086a8a8c543927de89115408d76e4d89d5
Ben Do <doben2150@gmail.com>
2020-11-21 15:49:34 -0600

    Initial Commit

---
+++ CreameryFlavery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreameryFlavery
+//
+//  Created by Ben Do on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
