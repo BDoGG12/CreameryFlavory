1ad250c3677b4b854ad3c45c4c0dea9f6badffef
Ben Do <doben2150@gmail.com>
2020-12-07 15:19:25 -0600

    New CoreML Model

--- CreameryFlavery/ViewController.swift
+++ CreameryFlavery/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Social
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    var classificationResults: [VNClassificationObservation] = []
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
     }
-
-
+    
+    func detect(image: CIImage) {
+        
+//        Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: IceCreamFlavorModel().model) else {
+            fatalError("Could not load ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not classify image.")
+            }
+            
+            self.navigationItem.title = classification.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage {
+            
+            imageView.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("Could not conver UIImage to CIImage")
+            }
+            detect(image: ciImage)
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+    return input.rawValue
+}
